a6ed33c9d839cf048f3f8d233963fcbab87b5bb4
Scott Torborg <scott@cartlogic.com>
2011-09-16 10:54:01 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="seolint",
-      version='0.1',
+      version='0.2',
       description="SEO linting tool.",
       long_description='',
       classifiers=[
